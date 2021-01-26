cd67126ccd168e245af0b5cffd1000888fdcfbea
M <programmer.ms.1997@gmail.com>
2021-01-26 23:21:05 +0300

    Created the memory structure and the memory initialization.

--- _6502_Emulator/main.cpp
+++ _6502_Emulator/main.cpp
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct CPU {
-	using Byte = unsigned char;
-	using Word = unsigned short;
+using Byte = unsigned char;
+using Word = unsigned short;
 
+using u32 = unsigned int;
+
+struct Mem
+{
+	static constexpr u32 MAX_MEM = 1024 * 64;
+	Byte Data[MAX_MEM];
+
+	void Initialize()
+	{
+		for (u32 i = 0; i < MAX_MEM; i++)
+		{
+			Data[i] = 0;
+		}
+	}
+};
+
+struct CPU
+{
 	Word PC; // Program Counter
 	Word SP; // Stack Pointer
 
@@ -22,19 +39,21 @@ struct CPU {
 	Byte V : 1;
 	Byte N : 1;
 
-	void Reset()
+	void Reset(Mem& memory)
 	{
 		// Faking the boot up sequence:
 		PC = 0xFFFC;		// 
 		SP = 0x0100;		// Setting the stack pointer
 		C = Z = I = D = B = V = N = 0; // Clearing the flags
 		A = X = Y = 0;		// 
+		memory.Initialize();
 	}
 };
 
 int main()
 {
+	Mem mem;
 	CPU cpu;
-	cpu.Reset();
+	cpu.Reset(mem);
 	return 0;
 }
