c438f563a9eccda8ef986b367846eb5b6f43382f
Muhammad N. Fadhil <muhammadnihad1997@gmail.com>
2024-08-30 18:23:27 +0300

    Moved the CPU class to its own dedicated header file.

---
+++ _6502_Emulator/cpu.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "memory.h"
+
+struct CPU
+{
+	Word PC; // Program Counter
+	Word SP; // Stack Pointer
+
+	// Registers
+	Byte A, // Accumulator
+		 X, // Index Register
+		 Y; // 
+
+	// Status Flags:
+	Byte C : 1;
+	Byte Z : 1;
+	Byte I : 1;
+	Byte D : 1;
+	Byte B : 1;	
+	Byte V : 1;
+	Byte N : 1;
+
+	void Reset(Memory& memory)
+	{
+		// Faking the boot up sequence:
+		PC = 0xFFFC;		// 
+		SP = 0x0100;		// Setting the stack pointer
+		C = Z = I = D = B = V = N = 0; // Clearing the flags
+		A = X = Y = 0;		// 
+		memory.Initialize();
+	}
+};
